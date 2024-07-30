07223f58bc9fe4d73eca0313a08c58b47c9db5a6
PlutoWooSeok <plutoxxx1014@gmail.com>
2024-07-30 16:02:15 +0900

    1주차 문제풀이 코드

---
+++ 0. 입출력 및 사칙연산/src/BOJ4375.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+//백준_4375_1
+public class BOJ4375 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // 입력이 있을 때까지 계속 반복합니다.
+        while (sc.hasNextInt()) {
+            int n = sc.nextInt();
+            int k = 0;
+
+            // i가 0부터 시작하여 무한 반복합니다.
+            for (int i = 0 ;; i++) {
+                k = (k * 10 + 1) % n;
+
+                // k가 0이 되면 현재 i 값에 1을 더해 출력하고 반복을 멈춥니다.
+                if (k == 0) {
+                    System.out.println(i + 1);
+                    break;
+                }
+            }
+        }
+        sc.close();
+    }
+}
