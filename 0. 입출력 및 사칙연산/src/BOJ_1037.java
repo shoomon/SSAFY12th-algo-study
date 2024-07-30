f240f8793f010f374f41118316792220a65ba36a
Bang <bangmq@naver.com>
2024-07-30 22:41:05 +0900

    240730

---
+++ 0. 입출력 및 사칙연산/src/BOJ_1037.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BOJ_1037 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		long[] nums = new long[n];
+		for (int i = 0; i < n; i++)
+			nums[i] = sc.nextInt();
+		
+		Arrays.sort(nums);
+		System.out.println(nums[0] * nums[n - 1]);
+	}
+}
