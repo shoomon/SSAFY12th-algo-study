ff9ac331901b2ae98a11b9c5fb15de0c512f16b9
shoomon <96561442+shoomon@users.noreply.github.com>
2024-08-14 10:21:43 +0900

    [solved] boj2609

--- 0. 입출력 및 사칙연산/src/BOJ2609/BOJ_2609_Wooseok.java
+++ 0. 입출력 및 사칙연산/src/BOJ2609/BOJ_2609_Wooseok.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.util.StringTokenizer;
-
+	
 public class BOJ_2609_Wooseok {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
