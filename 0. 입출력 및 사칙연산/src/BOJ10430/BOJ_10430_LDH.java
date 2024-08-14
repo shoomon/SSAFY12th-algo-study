d2aa8030f9419e75fa52c3ba0b4d6d3e4b650181
shoomon <96561442+shoomon@users.noreply.github.com>
2024-08-14 10:01:05 +0900

    aps

--- 0. 입출력 및 사칙연산/src/BOJ10430/BOJ_10430_LDH.java
+++ 0. 입출력 및 사칙연산/src/BOJ10430/BOJ_10430_LDH.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+	
 public class BOJ_10430_LDH {
     public static void main(String[] args) throws IOException{
         
