ca79e0e3724c757c0f0fcfb22eb5493c4f1ed43e
PlutoWooSeok <plutoxxx1014@gmail.com>
2024-08-21 14:30:04 +0900

    [solved]

--- 1. 브루트포스/src/BOJ2309/BOJ2309_wooseok.java
+++ 1. 브루트포스/src/BOJ2309/BOJ2309_wooseok.java
@@ -1,10 +1,12 @@
+package BOJ2309;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class Main {
+public class BOJ2309_wooseok {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         ArrayList<Integer> seven = new ArrayList<>();
