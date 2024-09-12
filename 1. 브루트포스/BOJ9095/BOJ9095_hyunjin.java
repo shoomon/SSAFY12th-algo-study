7b071e12016b9c83f3010e4ab62161a43e544194
Bang <bangmq@naver.com>
2024-09-12 10:21:15 +0900

    [solving] boj9095

--- 1. 브루트포스/BOJ9095/BOJ9095_hyunjin.java
+++ 1. 브루트포스/BOJ9095/BOJ9095_hyunjin.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 //BOJ9095_1,2,3 더하기
 // 안 풀려용,,,
 public class BOJ9095_hyunjin {
-	static int n, cnt;
+	static int n,  sum;
 	static int[] arr;
 	static int[] result;
 	static boolean[] visited;
@@ -17,33 +17,33 @@ public class BOJ9095_hyunjin {
 
 		for (int tc = 1; tc <= T; tc++) {
 			n = sc.nextInt();
-			cnt = 0;
 			result = new int[n];
 			visited = new boolean[arr.length];
 
+			sum = 0;
 			func(0, 0);
 
 		} // tc
 	} // main
 
-	static void func(int idx, int sum) {
+	static void func(int idx, int cnt) {
 
-		for (int i = 0; i < result.length; i++) {
-			sum += result[i];
-
-			if (sum == n) {
-				for (int a : result) {
-					System.out.print(a + " ");
-				}
-				System.out.println();
-				return;
+		if (sum == n) {
+			for (int a : result) {
+				System.out.print(a + " ");
 			}
+			System.out.println();
+			return;
+		}
+
+		if (sum > n) {
+			return;
 		}
 
 		for (int i = 0; i < 3; i++) {
 			result[cnt] = arr[i];
-			func(i, sum + 1);
-			func(i + 1, cnt + 1);
+			func(i, cnt + 1);
+
 		}
 	}
 }
