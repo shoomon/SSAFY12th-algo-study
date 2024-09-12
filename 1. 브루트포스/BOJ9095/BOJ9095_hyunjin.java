abc71d050104df96ab65dba0763f88f185c1a2af
Bang <bangmq@naver.com>
2024-09-12 14:34:30 +0900

    [solved] BOJ9095

--- 1. 브루트포스/BOJ9095/BOJ9095_hyunjin.java
+++ 1. 브루트포스/BOJ9095/BOJ9095_hyunjin.java
@@ -1,14 +1,14 @@
 package 브루트포스;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 //BOJ9095_1,2,3 더하기
-// 안 풀려용,,,
+
 public class BOJ9095_hyunjin {
-	static int n,  sum;
+	static int n, cnt;
 	static int[] arr;
 	static int[] result;
-	static boolean[] visited;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -18,21 +18,22 @@ public class BOJ9095_hyunjin {
 		for (int tc = 1; tc <= T; tc++) {
 			n = sc.nextInt();
 			result = new int[n];
-			visited = new boolean[arr.length];
+//			visited = new boolean[n];
 
-			sum = 0;
+			cnt = 0;
 			func(0, 0);
+			System.out.println(cnt);
 
 		} // tc
 	} // main
 
-	static void func(int idx, int cnt) {
-
+	static void func(int sum, int idx) {
 		if (sum == n) {
-			for (int a : result) {
-				System.out.print(a + " ");
-			}
-			System.out.println();
+//			for (int a : result) {
+//				System.out.print(a + " ");
+//			}
+//			System.out.println();
+			cnt++;
 			return;
 		}
 
@@ -40,10 +41,11 @@ public class BOJ9095_hyunjin {
 			return;
 		}
 
-		for (int i = 0; i < 3; i++) {
-			result[cnt] = arr[i];
-			func(i, cnt + 1);
-
+//		int tmp = -1;
+		for (int i = 1; i <= 3; i++) {
+			result[idx] = i;
+			func(sum + i, idx + 1);
+			result[idx] = 0;
 		}
 	}
 }
