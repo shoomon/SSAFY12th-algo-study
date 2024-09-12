205f56f2e7aef4e8cdaefef11b4c15c5f3a68d0a
shoomon <96561442+shoomon@users.noreply.github.com>
2024-09-12 13:05:46 +0900

    24.09.12

--- 1. 브루트포스/BOJ14500/BOJ14500_soomin.java
+++ 1. 브루트포스/BOJ14500/BOJ14500_soomin.java
@@ -2,11 +2,14 @@ package code;
 import java.util.*;
 import java.io.*;
 
-//24.09.11
-//설계 시간:
-//구현 시간:
+//24.09.12
+//설계 시간: 10분
+//구현 시간: 30분
+//메모리: 36112 kb
+//시간: 824 ms
 public class BOJ14500 {
-	static int N, M, map[][], maxVal[][];
+	static int N, M, map[][], answer;
+	static boolean[][] visited;
 	static int[] dx = {0, 1, 0, -1};
 	static int[] dy = {-1, 0, 1, 0};
 
@@ -18,7 +21,8 @@ public class BOJ14500 {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		map = new int[N][M];
-		maxVal = new int[N][M];
+		visited = new boolean[N][M];
+		answer = 0;
 		
 		for(int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -27,24 +31,50 @@ public class BOJ14500 {
 			}
 		}
 		
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < M; j++) {
+				DFS(i,j,0,0);
+				five(i,j);
+			}
+		}
+		System.out.println(answer);
 	}
 	
-	static void DFS(int curY, int curX) {
+	static void DFS(int curY, int curX, int depth, int sum) {
+		if(depth == 4) {
+			answer = Math.max(answer, sum);
+			return;
+		}
+		visited[curY][curX] = true;
 		
+		for(int i = 0; i < 4; i++) {
+			int nY = curY+dy[i];
+			int nX = curX+dx[i];
+			
+			if(check(nY,nX) && !visited[nY][nX]) {
+				DFS(nY,nX,depth+1,sum+map[curY][curX]);
+			}
+		}
+		visited[curY][curX] = false;
 	}
 	
 	static void five(int centerY, int centerX) {
-		for(int i = 0; i < 3; i++) {
-			for(int j = 0; j < 3; j++) {
+		int sum, max=0;
+		for(int i = 0; i < 4; i++) {
+			sum=map[centerY][centerX];
+			for(int j = 0; j < 4; j++) {
 				if(j == i) continue;
-				if(!check(centerY+dy[j], centerX+dx[j])) break;
+				if(!check(centerY+dy[j], centerX+dx[j])) continue;
+				sum += map[centerY+dy[j]][centerX+dx[j]];
 			}
+			max = Math.max(max, sum);
 		}
+		answer = Math.max(answer, max);
 	}
 	
 	static boolean check(int i, int j) {
-		if(-1 < i && i < N && -1 < j && j < M) return false;
-		return true;
+		if(-1 < i && i < N && -1 < j && j < M) return true;
+		return false;
 	}
 
 }
