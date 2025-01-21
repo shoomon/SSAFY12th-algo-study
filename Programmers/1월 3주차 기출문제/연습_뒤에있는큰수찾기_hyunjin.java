47ef3b8af2c50a6a17022d5da7ed883c5f96c24a
HyunjinKim <koujie11@gmail.com>
2025-01-21 23:36:18 +0900

    [PRO] 연습문제_뒤에 있는 큰 수 찾기

--- Programmers/1월 3주차 기출문제/연습_뒤에있는큰수찾기_hyunjin.java
+++ Programmers/1월 3주차 기출문제/연습_뒤에있는큰수찾기_hyunjin.java
@@ -1,5 +1,30 @@
 package PRO_1월_3주차;
 
-public class 연습_뒤에있는큰수찾기_hyunjin {
+import java.util.*;
 
+public class 연습_뒤에있는큰수찾기_hyunjin {
+	public static void main(String[] args) {
+		int[] numbers = { 9, 1, 5, 3, 6, 2 };
+		연습_뒤에있는큰수찾기_hyunjin sol = new 연습_뒤에있는큰수찾기_hyunjin();
+		System.out.println(Arrays.toString(sol.solution(numbers)));
+	}
+
+	public int[] solution(int[] numbers) {
+		int N = numbers.length;
+		int[] answer = new int[N];
+		Arrays.fill(answer, -1); // 모든 값을 -1로 초기화
+
+		Stack<Integer> stack = new Stack<>();
+
+		stack.push(0); // 첫 번째 인덱스 스택에 넣기
+
+		for (int i = 1; i < N; i++) {
+			while (!stack.isEmpty() && numbers[stack.peek()] < numbers[i]) {
+				answer[stack.pop()] = numbers[i];
+			}
+			stack.add(i);
+		}
+
+		return answer;
+	}
 }
