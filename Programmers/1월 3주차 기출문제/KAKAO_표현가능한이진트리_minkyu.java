abfe53e66bf930cd33a9202ee0ea850ccf4db4c7
방민규 <bangmq@naver.com>
2025-01-26 21:51:08 +0900

    [solved]

--- Programmers/1월 3주차 기출문제/KAKAO_표현가능한이진트리_minkyu.java
+++ Programmers/1월 3주차 기출문제/KAKAO_표현가능한이진트리_minkyu.java
@@ -4,7 +4,7 @@ import java.io.*;
 public class ExpressableBinaryTree {
     public static void main(String[] args) {
         Solution sol = new Solution();
-        long[] numbers = {99999999999999L, 42, 5};
+        long[] numbers = {15, 42, 5};
         int[] ans = sol.solution(numbers);
         System.out.println(Arrays.toString(ans));
     }
@@ -16,22 +16,32 @@ public class ExpressableBinaryTree {
             int[] answer = new int[numbers.length];
             int idx = 0;
             for (long number : numbers){
-                int curVal = 1;
                 int cnt = 0;
-                // 이진트리로 단계 검사
-                while (true){
-                    cnt = curVal - 1;
-                    if (Math.pow(2, curVal) - 1 < number) curVal *= 2;
+                while(true){
+                    if (Math.pow(2,cnt) - 1 < number) cnt++;
                     else break;
                 }
+                int level = 1;
+                // 이진트리로 단계 검사
+                while (true){
+                    if ((int)Math.pow(2,level) - 1 < cnt) level++;
+                    else break;
+                }
+                cnt = (int)Math.pow(2,level) - 1;
+
                 binaryNum = new int[cnt];
                 int pos = cnt - 1;
                 // 이진 수로 나타내기
+                int needCnt = 0;
                 while (number > 1){
                     // 나머지 넣어주기.
                     binaryNum[pos--] = (int)(number % 2);
+                    needCnt++;
                     number /= 2;
-                    if (number == 1) binaryNum[pos] = 1;
+                    if (number == 1){
+                        binaryNum[pos] = 1;
+                        needCnt++;
+                    }
                 }
 
                 isPossible = true;
