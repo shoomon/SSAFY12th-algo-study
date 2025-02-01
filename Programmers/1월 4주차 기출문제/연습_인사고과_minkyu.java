073dd4f9da17334959a9968eba1b83b9c35d7aaa
방민규 <bangmq@naver.com>
2025-02-01 23:02:49 +0900

    [solved]

---
+++ Programmers/1월 4주차 기출문제/연습_인사고과_minkyu.java
@@ -0,0 +1,15 @@
+public class PerformanceEvaluation {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[][] scores = {{2,2},{1,4},{3,2},{3,2},{2,1}};
+        int ans = sol.solution(scores);
+        System.out.println(ans);
+    }
+
+    static class Solution {
+        public int solution(int[][] scores) {
+            int answer = 0;
+            return answer;
+        }
+    }
+}
