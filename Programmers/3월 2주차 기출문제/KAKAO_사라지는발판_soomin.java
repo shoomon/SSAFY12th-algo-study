2671ffad5bc8620fec36c21b4e278b6e2a8c999a
shoomon <kate010117@naver.com>
2025-03-16 17:34:28 +0900

    25.03.16

---
+++ Programmers/3월 2주차 기출문제/KAKAO_사라지는발판_soomin.java
@@ -0,0 +1,11 @@
+//못 풀었어요
+
+class Solution {
+    int[] dY = {-1,0,1,0};
+    int[] dX = {0,1,0,-1};
+    
+    public int solution(int[][] board, int[] aloc, int[] bloc) {
+        int answer = -1;
+        return answer;
+    }
+}
